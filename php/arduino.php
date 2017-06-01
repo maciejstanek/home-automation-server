52ad051b50396ac3ab2afca3fdb3c5252c1cc871
maciej-stanek <makaron28@gmail.com>
2017-06-01 23:41:36 +0200

    wip

--- php/arduino.php
+++ php/arduino.php
@@ -1,3 +1,31 @@
 <?php
 
-echo 3;
+include_once "common.php";
+echo "test";
+
+if(isset($_POST['pir'])) {
+	$pir_sane_input = null;
+	if($_POST['pir'] === '1') {
+		$pir_sane_input = 1;
+	}
+	if($_POST['pir'] === '0') {
+		$pir_sane_input = 0;
+	}
+
+	if($pir_sane_input !== null) {
+		$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+		$stmt = $conn->prepare("insert into pir_log (status) values (?)");
+		$stmt->bind_param("i", $pir_sane_input);
+		$stmt->execute();
+		$stmt->close();
+		/*
+		$result = $stmt->get_result();
+		if($result->num_rows === 0){
+			$msg = "Nieprawidłowe dane logowania";
+		} else {
+			$_SESSION["uzytkownik"] = $result->fetch_assoc()['imie'];
+			header("Location: private.php");
+		}
+		*/
+	}
+}
--- php/common.php
+++ php/common.php
@@ -4,3 +4,9 @@ define("SERVER_PATH", "/var/www/html/mstanek/home_automation_server");
 define("SERVER_NAME", "Home Automation Server");
 
 define("NUMBER_OF_RELAYS", 3);
+
+define("DB_HOST", "localhost");
+define("DB_DATABASE", "home_automation_server");
+define("DB_USERNAME", "yellow");
+define("DB_PASSWORD", "pqqp");
+
